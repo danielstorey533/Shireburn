c31f70bb011f06e15691a1ceb2be04496d409031
Daniel Storey <danielstorey533@gmail.com>
2025-04-13 19:10:23 +0100

    Added PrimeVue, added PrimeVue styling, replaced list of employees with Datatable, simple pagination working

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue);
 
 app.mount('#app')
--- src/views/EmployeeDashboard.vue
+++ src/views/EmployeeDashboard.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
     <h2>Employees</h2>
-    <ul>
-      <li v-for="employee in employees" :key="employee.fullName">
-        {{ employee.fullName }} - {{ employee.occupation }} - {{ employee.department }}
-      </li>
-    </ul>
-    <div>
-      Page: {{ currentPage }}
-      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
-      <button @click="nextPage" :disabled="!hasNextPage">Next</button>
-    </div>
+    <DataTable
+  :value="employees"
+  :paginator="true"
+  :rows="10"
+  :lazy="true"
+  :totalRecords="totalRecords"
+  @page="onLazyLoad"
+>
+  <Column field="fullName" header="Name" sortable filter />
+  <Column field="occupation" header="Occupation" sortable filter />
+  <Column field="department" header="Department" sortable filter />
+  <Column field="employmentStatus" header="Employment Status">
+  <template #body="{ data }">
+    {{ renderEmploymentStatus(data) }}
+  </template>
+</Column>
+<Column
+  field="terminationStatus"
+  header="Termination Status"
+  :body="renderTerminationStatus"
+/>
+  <Column header="Actions" :body="renderActions" />
+</DataTable>
+
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, computed } from 'vue';
 import { useEmployeeStore } from '@/stores/employeeStore';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 const store = useEmployeeStore();
 
@@ -26,15 +42,26 @@ onMounted(() => {
 
 const employees = computed(() => store.paginatedEmployees);
 const currentPage = computed(() => store.currentPage);
+const pageSize = computed(() => store.pageSize);
+const totalRecords = computed(() => store.totalEmployees);
+
 const hasNextPage = computed(() => {
   return store.currentPage * store.pageSize < store.totalEmployees;
 });
 
-const nextPage = () => {
-  if (hasNextPage.value) store.setPage(store.currentPage + 1);
+const onLazyLoad = (event: any) => {
+  store.setPage(event.page + 1);
+  store.setPageSize(event.rows);
 };
 
-const prevPage = () => {
-  if (store.currentPage > 1) store.setPage(store.currentPage - 1);
+const renderEmploymentStatus = (rowData: any) => {
+  return 'Placeholder';
 };
+
+const renderTerminationStatus = (rowData: any) => {
+  return 'Placeholder';
+};
+
+
+
 </script>
